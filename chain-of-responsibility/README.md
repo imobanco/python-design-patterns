7ac2d186aa058c871c97f5b7ad55c0ab731028f0
ariane <arianesc@ufrn.edu.br>
2021-10-19 15:07:42 -0300

    altera exemplo

--- chain-of-responsibility/README.md
+++ chain-of-responsibility/README.md
@@ -1,2 +1,11 @@
 # Chain of Responsibility
 O Chain of Responsibility é um padrão de projeto comportamental que permite que você passe pedidos por uma corrente de handlers. Ao receber um pedido, cada handler decide se processa o pedido ou o passa adiante para o próximo handler na corrente.
+
+
+###Fontes:
+- https://refactoring.guru/pt-br/design-patterns/chain-of-responsibility
+
+- https://www.youtube.com/watch?v=fG5pYDVm8_M
+
+- https://www.youtube.com/watch?v=iqVOUAbeGoE
+
---
+++ chain-of-responsibility/loja.py
@@ -0,0 +1,102 @@
+from typing import Callable
+
+class Item:
+    def __init__(self, nome: str, valor: int):
+        self.nome = nome
+        self.valor = valor
+
+    def __repr__(self):
+        return f'Item({self.nome}, {self.valor})'
+
+class Carrinho:
+    def  __init__(self):
+        self.itens = []
+
+    def adicionar_item(self, item: Item):
+        self.itens.append(item)
+
+    @property
+    def valor(self):
+        return sum(map(lambda item: item.valor, self.itens))
+
+def promocao_30(carrinho: Carrinho):
+    if carrinho.valor > 1000:
+        return carrinho.valor - (carrinho.valor * 0.3)
+
+def promocao_10(carrinho: Carrinho):
+    if len(carrinho.itens) >= 5:
+        return carrinho.valor - (carrinho.valor * 0.10)
+
+def sem_promocao(carrinho: Carrinho):
+    return carrinho.valor
+
+# Handler
+class Promocoes:
+    def __init__(self, *promos: Callable):
+        self.promos = promos
+        self.sem_promocao = sem_promocao
+
+    def calcular(self, valor: int):
+        for promo in self.promos:
+            if (resultado := promo(valor)):
+                return resultado
+
+        return self.sem_promocao(valor)
+
+c = Carrinho()
+
+p = Promocoes(promocao_30, promocao_10)
+
+c.adicionar_item(Item('banana', 10))
+c.adicionar_item(Item('banana', 10))
+c.adicionar_item(Item('banana', 10))
+c.adicionar_item(Item('banana', 10))
+c.adicionar_item(Item('banana', 10))
+
+print("Exemplo 1: O carrinho possui 5 itens, logo, a promoção que irá ser aplicada é a promoção de 10%")
+
+print(f"Carinho {c.itens}")
+
+print(f"valor original: {c.valor}")
+
+total = p.calcular(c)
+
+print(f"valor final: {total}")
+
+print(total==45)
+
+print("--------------------------------------------")
+
+print("Exemplo 2: O valor total da compra é mais de 1000, logo, a promoção que irá ser aplicada é a promoção de 30%")
+
+c2 = Carrinho()
+
+c2.adicionar_item(Item('banana', 1001))
+
+print(f"Carinho {c2.itens}")
+
+print(f"valor original: {c2.valor}")
+
+total = p.calcular(c2)
+
+print(f"valor final: {total}")
+
+print(total== 700.7)
+
+print("--------------------------------------------")
+
+print("Exemplo 3: O carrinho não possui 5 compras e não possui valor total de 1000, logo, não será aplicada nenhuma promoção")
+
+c3 = Carrinho()
+
+c3.adicionar_item(Item('banana', 1))
+
+print(f"Carinho {c3.itens}")
+
+print(f"valor original: {c3.valor}")
+
+total = p.calcular(c3)
+
+print(f"valor final: {total}")
+
+print(total== 1)
