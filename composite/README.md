305efe9ab642c4ec92ada709499f0d4c09cb74da
ariane <arianesc@ufrn.edu.br>
2021-10-20 14:35:14 -0300

    adiciona exemplo e altera o readme

--- composite/README.md
+++ composite/README.md
@@ -1,4 +1,21 @@
 # Composite 
 #### Árvore de objetos, Object tree
 
-O Composite é um padrão de projeto estrutural que permite que você componha objetos em estruturas de árvores e então trabalhe com essas estruturas como se elas fossem objetos individuais.
+O Composite é um padrão de projeto estrutural que permite que você componha objetos em estruturas de árvores 
+e trabalhe com essas estruturas como se elas fossem objetos individuais.
+
+Foi usado como exemplo uma empresa composta por departamentos que são compostos por funcionários.
+
+- Funcionário: Representa o objeto folha, a folha não tem nenhum componente filho.
+- Departamento e Empresa: Armazena o componente filho e implementa funções relacionadas ao componente filho.
+
+## Para rodar os exemplos:
+``` 
+python composite/empresa.py  
+```
+
+### Fontes:
+- https://refactoring.guru/pt-br/design-patterns/composite/python/example
+- https://www.geeksforgeeks.org/composite-method-python-design-patterns/
+
+
---
+++ composite/empresa.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class Funcionario:
+    """Classe que representa a Folha da árvore hierárquica."""
+    def __init__(self, nome):
+        self.nome = nome
+
+    def __repr__(self):
+        return self.nome
+
+class Departamento:
+    """ Classe que representa componentes complexos que podem ter folhas."""
+    def __init__(self, nome):
+        self.nome = nome
+        self._children = []
+
+    def add(self, funcionario:Funcionario):
+        self._children.append(funcionario)
+
+    def __repr__(self):
+        return self._children
+
+class Empresa:
+    """ Classe que representa componentes complexos que podem ter folhas."""
+
+    def __init__(self, nome):
+        self._children = []
+        self.nome = nome
+
+    def add(self, departamento:Departamento):
+        self._children.append(departamento)
+
+
+
+print ("--Exemplo--")
+
+f1 = Funcionario(nome="Ana")
+f2 = Funcionario(nome="Maria")
+f3 = Funcionario(nome="Luiz")
+d1 = Departamento(nome="RH")
+
+print(f"três funcionários {f1.nome, f2.nome, f3.nome} do departamento {d1.nome}")
+
+d1.add(f1)
+d1.add(f2)
+d1.add(f3)
+
+f4 = Funcionario(nome="Cesar")
+f5 = Funcionario(nome="Wilson")
+f6 = Funcionario(nome="Luiza")
+
+d2 = Departamento(nome="TI")
+
+print(f"três funcionários {f4.nome, f5.nome, f6.nome} do departamento {d2.nome}")
+
+d2.add(f4)
+d2.add(f5)
+d2.add(f6)
+
+
+e1 = Empresa(nome="Imobanco")
+
+e1.add(d1)
+e1.add(d2)
+
+print(f"Os dois departamentos {d1.nome, d2.nome} pertecem a empresa {e1.nome}")
