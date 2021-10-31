86f9f9b7a748c0a9cd32cf9c68eec17fd151ce8f
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2021-10-31 19:00:44 -0300

    Remove métodos desnecéssario

--- observer/observer_concreto.py
+++ observer/observer_concreto.py
@@ -5,27 +5,10 @@ class BolaDeCristal(Observavel):
         print(f"Fausto está na escola, mas recebeu a mensagem: {mensagem}")
 
 class Centauro(Observador):
-    def __init__(self):
-        self._observers = []
-
-    def adicionar_observer(self, observador):
-        self._observers.append(observador)
-
-    def notificar_observers(self, mensagem):
-        for observador in self._observers:
-            observador.atualizar(mensagem)
+    ...
 
 class Unicornio(Observador):
-    def __init__(self):
-        self._observers = []
-
-    def adicionar_observer(self, observador):
-        self._observers.append(observador)
-
-    def notificar_observers(self, mensagem):
-        for observador in self._observers:
-            observador.atualizar(mensagem)
-
+    ...
 
 unicornio = Unicornio()
 unicornio.adicionar_observer(BolaDeCristal())
