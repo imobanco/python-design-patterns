68c223d7aaef0fd323c1832a8fe4c509d97a1e5e
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2021-10-31 18:42:45 -0300

    Update observer/observer_abc.py
    Co-authored-by: Rodrigo Castro <rodrigondec@gmail.com>

--- observer/observer_abc.py
+++ observer/observer_abc.py
@@ -1,16 +1,12 @@
 from abc import ABC, abstractmethod, abstractproperty
 from typing import List
 
-class Observador(ABC):
+class Observavel(ABC):
     """Feitiço para objetos que querem ser observados."""
 
     @abstractmethod
     def atualizar(self):
         ...
-
-
-class Observavel(ABC):
-    """Feitiço observar objetos observáveis."""
 
     @property
     @abstractmethod
@@ -23,4 +19,11 @@ class Observavel(ABC):
 
     @abstractmethod
     def notificar_observers(self, mensagem):
+        ...
+
+class Observador(ABC):
+    """Súditos que observam algo!""""
+
+    @abstractmethod
+    def receber_notificacao(self, mensagem):
         ...
